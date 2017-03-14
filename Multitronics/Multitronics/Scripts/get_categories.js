9517216844ff529f1e72fbce34de576de9662969
Garry_Jane <mmm53.ripe@gmail.com>
2017-03-14 16:26:27 +0300

    Creating new script file

--- Multitronics/Multitronics/Scripts/get_categories.js
+++ Multitronics/Multitronics/Scripts/get_categories.js
@@ -1,41 +1,70 @@
 /**
- * Created by ddl on 13.03.17.
- */
-'use strict';
-
-/**
- * Скрипт для странички товаров.
- * Отправляет GET запрос по адресу "/GetCategories"
- * получает JSON список категорий, и отображает их в виде кнопок (временно просто кнопок сверху).
+ * Скрипт отправляет GET запрос по адресу "/GetCategories",
+ * получает JSON списка категорий,
+ * отображает их в виде кнопок.
+ *
  * По нажатию на любую из кнопок отправляет GET по адресу "/ProductsData?Category=XXX",
- * где XXX - ID нужной категории (в кнопке отображается имя, а по нажатию отправляется ID той,
- * которую выбрал пользователь).
- * Далее получает JSON всех товаров, и отображаете их.
- **/
+ * где XXX - ID нужной категории (XXX = name кнопки),
+ * полученный ответ на данном этапе выводится в консоль.
+ *
+ * @requires vue@2.2.4.js
+ * @requires vue-resource.min.js
+ *
+ * Тестировал на:
+ *...
+ *<div id="nav">
+ *  <button v-for="(cat, index) in categories" :name="index" @click="catBtnOnClick">
+ *     {{ cat }}
+ *</button>
+ *</div>
+ * ...
+ * */
 
 'use strict';
 
-var testJSON = {
-	cat1: "cat1",
-	cat2: "cat2"
-};
-
-testJSON = JSON.stringify(testJSON);
-
-function getCategories(event) {
-	event.preventDefault();
-	var str = ($( testJSON ).serialize());
-	$.ajax({
-		url: '/GetCategories',
-		type: 'GET',
-		dataType: 'json',
-		success: renderNavBtns,
-		data: str
-	});
-}
-
-function renderNavBtns() {
-	console.log('render');
-}
-
-console.log(testJSON);
+let nav = new Vue({
+	el: '#nav',
+	data: {
+		/* Категории по умолчанию */
+		defCat: {
+			defCat1: "def-cat1-value",
+			defCat2: "def-cat2-value",
+			defCat3: "def-cat3-value",
+			defCat4: "def-cat4-value"
+		},
+		categories: this.defCat,
+	},
+	methods: {
+		/* Выполняется, если результат запроса - ошибка */
+		getCatError: function () {
+			this.categories = this.defCat;
+		},
+		/**
+		 * @description Запрос методом GET, результат присваивается this.categories
+		 * @return DOM
+		 * */
+		getCat: function () {
+			this.$http.get('/GetCategories')
+				.then((response) => {
+					this.categories = response.body;
+				})
+				.catch(this.getCatError);
+		},
+		/**
+		 * @description Обработчик нажатия на кнопку "Категория"
+		 * @return DOM
+		 * */
+		catBtnOnClick: function (event) {
+			console.log(event.target.name);
+			this.$http.get('/ProductsData?Category=' + event.target.name)
+			/**
+			 * @todo Результат запроса следует присвоить переменной, влияющей на представление продуктов, пока - в консоль.
+			 * */
+				.then((response) => {
+					console.log(response.body);
+				}).bind(this)
+				.catch(console.log(`No response for request /ProductsData?Category='${(event.target.name)}`));
+		}
+	}
+});
+nav.getCat();
