05c32238a93aad3ed7c025d7a80dd0f8ddff7ec0
Olexandr Panov <futupas@gmail.com>
2017-03-25 19:27:31 +0200

    ConsoleDataWriter

---
+++ Multitronics/ConsoleDatawriter/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace ConsoleDatawriter
+{
+    class Program
+    {
+        static XmlDocument SettingsFile;
+        static void Main(string[] args)
+        {
+            Initialize("");
+            //Console.WriteLine(Environment.GetEnvironmentVariable("APPDATA")+"\\");
+            Console.ReadLine();
+        }
+        static void Initialize(string str)
+        {
+            string SettingsFilePath = (Environment.GetEnvironmentVariable("APPDATA")+@"\MultitronicsDataSetter\");
+            string SettinsFileName = "data.xml";
+
+            if (!Directory.Exists(SettingsFilePath))
+            {
+                Directory.CreateDirectory(SettingsFilePath);
+            }
+            if (!File.Exists(SettingsFilePath + SettinsFileName))
+            {
+                File.Create(SettingsFilePath + SettinsFileName);
+            }
+
+            start:
+            try
+            {
+                SettingsFile = new XmlDocument();
+                SettingsFile.Load(SettingsFilePath + SettinsFileName);
+                XmlElement xRoot = SettingsFile.DocumentElement;
+                string connectionString = xRoot["connectionString"].InnerText;
+                Console.WriteLine(connectionString);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("NullReferenceException");
+                SettingsFile = new XmlDocument();
+                SettingsFile.Load(SettingsFilePath + SettinsFileName);
+                XmlElement xRoot = SettingsFile.DocumentElement;
+                XmlElement connStr = SettingsFile.CreateElement("connectionString");
+                connStr.InnerText = "cccccccccccccccccccccccc";
+                xRoot.AppendChild(connStr);
+                SettingsFile.Save(SettingsFilePath + SettinsFileName);
+                goto start;
+            }
+            catch (XmlException ex)
+            {
+                Console.WriteLine("XmlException");
+                File.WriteAllText((SettingsFilePath + SettinsFileName), "<?xml version='1.0' encoding='utf-8' ?><root></root>", Encoding.UTF8);
+                goto start;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+
+            //Console.WriteLine(SettingsFilePath + SettinsFileName);
+        }
+    }
+}
+
+/*
+ 
+ <?xml version="1.0" encoding="utf-8" ?>
+<root>
+  <connectionString>Conn str ddfdfdfdfdfdfdfd</connectionString>
+  <registerKey>dddddddddddddd</registerKey>
+</root>
+ 
+ */
---
+++ Multitronics/ConsoleDatawriter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleDatawriter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("ConsoleDatawriter")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e46fe066-6587-47a4-897e-d5886785f8e8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
